e8b7ad1aff2a77a970e3aaf46c476171556414c0
Ruto <ageevaa50@gmail.com>
2025-05-03 18:51:12 +0800

    graf

--- test.js
+++ test.js
@@ -10,6 +10,7 @@
         const container = document.getElementById('input');
         container.innerHTML='';
         const table = document.createElement('table');
+        table.id='table';
 
         let row =document.createElement('tr');
         for (let j = 0; j <= n; j++) {
@@ -23,7 +24,8 @@
 
             for (let j = 0; j <= n; j++) {
                 const cell = document.createElement(j === 0 ? 'th' : 'td');
-                cell.textContent = j === 0 ? 'v'+i : 0;
+                cell.textContent = j === 0 ? 'v'+i : j == i ? '' : 0;
+                cell.addEventListener("click", click);
                 row.appendChild(cell);
             }
 
@@ -31,5 +33,57 @@
         }
 
         container.appendChild(table);
+        createGraf();
+    }
+    function createGraf(){
+        const output = document.getElementById('output');
+        output.innerHTML='';
+        let n = document.getElementById('in').value;
+        for (let i = 1; i <= n; i++) {
+            const ball = document.createElement('div');
+            ball.className='ball';
+            ball.innerText=i;
+            ball.style.left=Math.random()*(output.offsetWidth-50)+output.offsetLeft+"px";
+            ball.style.top=Math.random()*(output.offsetHeight-50)+output.offsetTop+"px";
+            output.append(ball);
+        }
+        change();
+    }
+    function click()
+    {
+        this.innerText= this.innerText=='' ? '': '0' ? '1' : '0' ;
+        let cell = this.cellIndex;
+        let row = this.parentNode.rowIndex;
+        document.getElementById("table").rows[cell].cells[row].innerText=this.innerText;
+        change();
+    }
+    function change()
+    {
+        const n = document.getElementById('in').value;
+        const table=document.getElementById("table");
+        const balls=document.getElementById("output").children;
+        let canvas = document.getElementById("canvas");
+        let ctx = canvas.getContext("2d");
+        console.log(balls[1]);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for(let i=1;i<=n;i++)
+        {
+            for(let j=i+1;j<=n;j++)
+            {
+                if (table.rows[i].cells[j].innerText=='1')
+                {
+                    let x1 = balls[i-1].offsetLeft+25-canvas.style.left; // X-координата первой точки
+                    let y1 = balls[i-1].offsetTop+25-canvas.style.top; // Y-координата первой точки
+                    let x2 = balls[j-1].offsetLeft+25-canvas.style.left; // X-координата второй точки
+                    let y2 = balls[j-1].offsetTop+25-canvas.style.top; // Y-координата второй точки
+                    console.log(x1,x2,y1,y2);
+                    console.log(canvas.offsetTop);
+                    ctx.beginPath(); // Начинаем новый путь
+                    ctx.moveTo(x1, y1); // Перемещаемся к первой точке
+                    ctx.lineTo(x2, y2); // Рисуем линию ко второй точке
+                    ctx.stroke(); // Отображаем линию
+                }
+            }
+        }
     }
 })();
