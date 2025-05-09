d919c9c271ea6907e2bb404c9b4e5364a08be227
Darya-Rusanova <dr1305@bk.ru>
2025-05-09 19:33:38 +0800

    +icon

---
+++ js/setWidth.js
@@ -0,0 +1,99 @@
+(function () {
+    window.addEventListener('load', init);
+
+    function init() {
+        document.getElementById('in').addEventListener('change', generateTable);
+    }
+
+    function generateTable(){
+        let n = document.getElementById('in').value;
+        const container = document.getElementById('input');
+        container.innerHTML='';
+        const table = document.createElement('table');
+        table.id='table';
+        if (n > 15){
+            table.style.fontSize="17px";
+        }
+
+        let row =document.createElement('tr');
+        for (let j = 0; j <= n; j++) {
+            const cell = document.createElement('th');
+            cell.textContent =  j === 0 ? '' : 'v'+j; 
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+        for (let i = 1; i <= n; i++) {
+            const row = document.createElement('tr');
+
+            for (let j = 0; j <= n; j++) {
+                const cell = document.createElement(j === 0 ? 'th' : 'td');
+                if(j === 0) {
+                    cell.textContent ='v'+i;
+                }else if (j == i){
+                    cell.textContent ='';
+                }else{
+                    const input = document.createElement('input');
+                    input.type = 'text';
+                    input.className = 'table-input';
+                    cell.appendChild(input);
+                    if (n > 12){
+                        input.style.width="20px";
+                    }
+                }
+                row.appendChild(cell);
+            }
+
+            table.appendChild(row);
+        }
+
+        container.appendChild(table);
+        createGraf();
+        document.getElementById('start').style.visibility = 'visible';
+        document.getElementsByClassName('way')[0].style.visibility = 'visible';
+    }
+    function createGraf(){
+        const output = document.getElementById('output');
+        let n = document.getElementById('in').value;
+        output.innerHTML='';
+        const radius = 180;
+        for (let i = 1; i <= n; i++) {
+            const ball = document.createElement('div');
+            ball.className='ball';
+            ball.innerText=i;
+            const angle = (i / n) * (2 * Math.PI);
+            ball.style.left=radius * Math.cos(angle) + (output.offsetWidth / 2)+"px";
+            ball.style.top=radius * Math.sin(angle) + (output.offsetHeight / 2)+"px";
+            output.append(ball);
+        }
+        change();
+    }
+    function change()
+    {
+        const n = document.getElementById('in').value;
+        const table=document.getElementById("table");
+        const balls=document.getElementById("output").children;
+        let canvas = document.getElementById("canvas");
+        let ctx = canvas.getContext("2d");
+        console.log(balls[1]);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for(let i=1;i<=n;i++)
+        {
+            for(let j=i+1;j<=n;j++)
+            {
+                if (table.rows[i].cells[j].innerText=='1')
+                {
+                    let x1 = balls[i-1].offsetLeft+25-canvas.style.left; // X-координата первой точки
+                    let y1 = balls[i-1].offsetTop+25-canvas.style.top; // Y-координата первой точки
+                    let x2 = balls[j-1].offsetLeft+25-canvas.style.left; // X-координата второй точки
+                    let y2 = balls[j-1].offsetTop+25-canvas.style.top; // Y-координата второй точки
+                    console.log(x1,x2,y1,y2);
+                    console.log(canvas.offsetTop);
+                    ctx.beginPath(); // Начинаем новый путь
+                    ctx.moveTo(x1, y1); // Перемещаемся к первой точке
+                    ctx.lineTo(x2, y2); // Рисуем линию ко второй точке
+                    ctx.stroke(); // Отображаем линию
+                }
+            }
+        }
+    }
+})();
