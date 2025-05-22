129980f6cce5a555c66bb617c186dc46b6397f2a
Ruto <ageevaa50@gmail.com>
2025-05-22 18:30:31 +0800

    10

--- js/tenth.js
+++ js/tenth.js
@@ -3,12 +3,15 @@
 
     function init() {
         document.getElementById('add').addEventListener('click', addBall);
-        document.getElementById('delete').addEventListener('click', orchiectomy);
-        //document.getElementById("start").addEventListener('click',daic)
+        document.getElementById('delete').disabled = true;
+        document.getElementById("start").addEventListener('click', pruf)
         canvas = document.getElementById("canvas");
         ctx = canvas.getContext("2d");
     }
-    let g= new Map;
+    let g= [];
+    for (let i=0;i<=20;i++){
+        g.push([]);
+    }
     let l=[];
     l.push([]);
     let used = new Set;
@@ -18,24 +21,25 @@
         this.className="checked ball";
     }
     function addBall(){
-        if (l[0].length != 0) {
-            let prev= document.getElementsByClassName("checked")[0];
-            if (!used.has(Number(document.getElementById("in").value))){
-                if (!g.has(Number(prev.innerHTML))) g.set(Number(prev.innerHTML), []);
-                if (l.length-1 == findIndex2D(l,Number(prev.innerHTML))[0]) l.push([]);
-                l[findIndex2D(l,Number(prev.innerHTML))[0]+1].push(Number(document.getElementById("in").value));
-                g.get(Number(prev.innerHTML)).push(Number(document.getElementById("in").value));
+        if (document.getElementById("in").value<=21){
+            if (l[0].length != 0) {
+                let prev= document.getElementsByClassName("checked")[0];
+                if (!used.has(Number(document.getElementById("in").value))){
+                    if (l.length-1 == findIndex2D(l,Number(prev.innerHTML))[0]) l.push([]);
+                    l[findIndex2D(l,Number(prev.innerHTML))[0]+1].push(Number(document.getElementById("in").value));
+                    g[Number(prev.innerHTML)].push(Number(document.getElementById("in").value));
+                    g[Number(document.getElementById("in").value)].push(Number(prev.innerHTML));
+                    used.add(Number(document.getElementById("in").value));
+                    createBall();
+                }
+            }
+            else {
+                l[0].push(Number(document.getElementById("in").value));
                 used.add(Number(document.getElementById("in").value));
                 createBall();
             }
+            changePos();
         }
-        else {
-            l[0].push(Number(document.getElementById("in").value));
-            g.set(-1, [Number(document.getElementById("in").value)]);
-            used.add(Number(document.getElementById("in").value));
-            createBall();
-        }
-        changePos();
     }
     function createBall(){
         const ball = document.createElement('div');
@@ -48,28 +52,27 @@
     function orchiectomy()
     {
         let victim = Number(document.getElementById("in").value);
-        console.log(victim);
-        for (const [key,value] of g) {
-            newVal = g.get(key).filter(num => num !== victim);
-            g.set(key, newVal);
-        }
         deleteRec(victim);
-        changePos();
-    }
-    function deleteRec(victim){
-        for (let[key, value] of g) {
-            console.log(key,value);
-            if (key === victim)
+        for (let i =0;i<g.length;i++) {
+            for( let j=0;j<g[i].length;j++)
             {
-                for(let j=0;j<value.length;j++)
-                {
-                    g.delete(j)
-                    deleteRec(value[j]);
-                }
+                if (g[i][j]==victim) g[i].splice(j,1);
             }
         }
-        g.delete(victim);
-        console.log(l,victim);
+        changePos();
+        console.log(g,l,used);
+    }
+    function deleteRec(victim){
+        let y= g[victim];
+        for (let i=0;i<y.length;i++) {
+            console.log(g[victim].length);
+            console.log(victim,findIndex2D(l,victim)[0],g[victim][i],findIndex2D(l,g[victim][i])[0]);
+            if (findIndex2D(l,victim)[0]<findIndex2D(l,y[i])[0])
+            {
+                deleteRec(y[i]);
+                g[victim]=[];
+            }
+        }
         l[findIndex2D(l,victim)[0]].splice(findIndex2D(l,victim)[1],1);
         used.delete(victim);
         document.getElementById(victim).remove();
@@ -99,14 +102,10 @@
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.strokeStyle='black';
         ctx.lineWidth = 1;
-        for (const [key, value] of g) {
-            if (key!=-1)
+        for (let i=0;i<g.length;i++) {
+            for(let j=0;j<g[i].length;j++)
             {
-                for(let j=0;j<value.length;j++)
-                {
-                    console.log(g)
-                    drawLine(document.getElementById(key),document.getElementById(value[j]));
-                }
+                drawLine(document.getElementById(i),document.getElementById(g[i][j]));
             }
         }
     }
@@ -120,5 +119,57 @@
         ctx.moveTo(x1, y1); 
         ctx.lineTo(x2, y2);
         ctx.stroke(); 
+    }
+
+    function pruf(){
+        let n = document.getElementsByClassName("ball").length;
+        for(let i=1;i<=n;i++)
+        {
+            if (g[i].length===0) alert("пизда те копченый пиши нормально");
+        }
+        let degree=[];
+        let killed=[];
+        
+        for (let i=0;i<=20;i++){
+            degree.push([]);
+            killed.push([]);
+        }
+        let leaves = [];
+        for (let i=0; i<=20; ++i) {
+            degree[i] = g[i].length;
+            console.log(degree[i],g[i].length);
+            if (degree[i] === 1)
+                leaves.push(i);
+            killed[i] = false;
+        }
+    
+        let result =[];
+        for (let iter=0; iter<n-2; ++iter) {
+            leaves.sort(compare);
+            console.log(result,leaves);
+            let leaf = leaves[0];
+            leaves.splice(0,1);
+            killed[leaf] = true;
+    
+            let v;
+            for (let i=0; i<g[leaf].length; ++i)
+                if (!killed[g[leaf][i]])
+                    v = g[leaf][i];
+    
+            result.push(v);
+            if (--degree[v] == 1)
+                leaves.push(v);
+        }
+        document.getElementById("res").innerText=result;
+    }
+    
+    function compare( a, b ) {
+        if ( a < b){
+            return -1;
+        }
+        if ( a> b ){
+            return 1;
+        }
+        return 0;
     }
 })();
